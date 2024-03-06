b807d9ec051b5afb96525a4c606c813e66cceeee
JenChieh <jcs090218@gmail.com>
2024-03-06 02:40:42 -0800

    Try hm

--- test/main.c
+++ test/main.c
@@ -21,13 +21,43 @@ void try_arr() {
     arrfree(arr);
 }
 
+typedef struct {
+    int  key;
+    int* value;
+} hm_int;
+
+void try_hm() {
+    hm_int* hm = NULL;
+    int*    elm = NULL;
+    int*    elm2 = NULL;
+
+    arrput(elm, 1);
+    arrput(elm, 2);
+
+    int key1 = 0;
+    hmput(hm, key1, elm);
+
+    int key2 = 9;
+    hmput(hm, key2, elm2);
+
+    for (int i = 0; i < hmlen(hm); ++i) {
+        printf("%d", hm[i].key);
+    }
+
+    arrfree(elm2);
+    arrfree(elm);
+    hmfree(hm);
+
+}
+
 int main(int argc, char* argv[]) {
     flx_result result;
     flx_score(&result, "buffer-file-name", "bfn");
     printf("Score %d\n", result.score);
     printf("Indicies %d\n", result.indices[0]);
-
     flx_free(&result);
+
+    //try_hm();
 
     return 0;
 }
