888974c0ef3dce09c9de78f46c13e0958853298b
JenChieh <jcs090218@gmail.com>
2024-03-06 04:22:02 -0800

    test

--- test/main.c
+++ test/main.c
@@ -21,43 +21,30 @@ void try_arr() {
     arrfree(arr);
 }
 
-typedef struct {
-    int  key;
-    int* value;
-} hm_int;
-
-void try_hm() {
-    hm_int* hm = NULL;
-    int*    elm = NULL;
-    int*    elm2 = NULL;
-
-    arrput(elm, 1);
-    arrput(elm, 2);
-
-    int key1 = 0;
-    hmput(hm, key1, elm);
-
-    int key2 = 9;
-    hmput(hm, key2, elm2);
-
-    for (int i = 0; i < hmlen(hm); ++i) {
-        printf("%d", hm[i].key);
-    }
-
-    arrfree(elm2);
-    arrfree(elm);
-    hmfree(hm);
-
+void modify_arr(int** arr) {
+    *arr = NULL;
+    arrput(*arr, 1);
+    arrput(*arr, 2);
 }
 
-int main(int argc, char* argv[]) {
+void test_flx() {
     flx_result result;
     flx_score(&result, "buffer-file-name", "bfn");
     printf("Score %d\n", result.score);
     printf("Indicies %d\n", result.indices[0]);
     flx_free(&result);
+}
 
-    //try_hm();
+int main(int argc, char* argv[]) {
+    //test_flx();
+
+    int* arr = NULL;
+
+    modify_arr(&arr);
+
+    printf("|%s\n", arrlen(arr));
+
+    arrfree(arr);
 
     return 0;
 }
