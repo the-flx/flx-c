87d53895461d16cb81fac36bad498cd413e1fa35
JenChieh <jcs090218@gmail.com>
2025-12-31 00:23:40 +0800

    fix: eol

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,19 @@
-#!/bin/bash# ========================================================================# $File: build.sh $# $Date: 2024-03-06 16:49:29 $# $Revision: $# $Creator: Jen-Chieh Shen $# $Notice: See LICENSE.txt for modification and distribution information#                   Copyright © 2024 by Shen, Jen-Chieh $# ========================================================================cd ..echoecho ::Build ninjacmake -S . -B build/ninja/ -GNinjaechoecho ::Build macOScmake -S . -B build/macos/
+#!/bin/bash
+# ========================================================================
+# $File: build.sh $
+# $Date: 2024-03-06 16:49:29 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2024 by Shen, Jen-Chieh $
+# ========================================================================
+
+cd ..
+
+echo
+echo ::Build ninja
+cmake -S . -B build/ninja/ -GNinja
+
+echo
+echo ::Build macOS
+cmake -S . -B build/macos/
